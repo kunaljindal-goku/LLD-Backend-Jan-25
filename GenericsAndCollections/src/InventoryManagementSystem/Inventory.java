bd16974e2fdf6e299e0514bd2cd19b585bed4384
Kunal Jindal <kunaljindal@Kunals-MacBook-Pro.local>
2026-02-26 22:21:52 +0530

    Inventory Mangement system - further milestones

--- GenericsAndCollections/src/InventoryManagementSystem/Inventory.java
+++ GenericsAndCollections/src/InventoryManagementSystem/Inventory.java
@@ -1,32 +1,61 @@
 package InventoryManagementSystem;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
+import java.util.*;
 
-public class Inventory<T extends Item>{
-
+public class Inventory<T extends Item> {
     private HashMap<String, T> items;
 
     public Inventory() {
-        this.items = new HashMap<>();
+        items = new HashMap<>();
     }
 
-
-    public void addItem(T item) {
-        items.put(item.getId(),item);
+    public void add(T item) {
+        if(item.getQuantity() < 0){
+     //       throw new InvalidQuantityException("Quantity is less than equal to zero which is invalid");
+            return;
+        }
+        if(items.containsKey(item.getId())) {
+//            System.out.println("Item " + item.getId() + " already exists");
+     //       throw new DuplicateItemException("Item " + item.getId() + " already exists");
+        }
+        items.put(item.getId(), item);
     }
 
-    public void removeItem(String id) {
-        items.remove(id);
+    public void remove(T item) {
+        items.remove(item.getId());
     }
 
-    public T getItem(String id) {
+    public T get(String id) {
         return items.get(id);
     }
 
-    public Collection<T> getAllItems() {
+    public List<T> getAll() {
         return new ArrayList<>(items.values());
     }
 
+    public List<T> filterByPrice(double minPrice , double maxPrice) {
+        List<T> filtered = new ArrayList<>();
+        for(T item : items.values()) {
+            if(item.getPrice() >= minPrice && item.getPrice() <= maxPrice) {
+                filtered.add(item);
+            }
+        }
+        return filtered;
+    }
+
+    public List<T> filterByAvailability() {
+        List<T> filtered = new ArrayList<>();
+        for(T item : items.values()) {
+            if (item.getQuantity() > 0){
+                filtered.add(item);
+            }
+        }
+        return filtered;
+    }
+
+    public List<T> sortItems(Comparator<T> comparator){
+        List<T> sortedItems = new ArrayList<>(items.values());
+        Collections.sort(sortedItems , comparator);
+        return sortedItems;
+    }
 }
