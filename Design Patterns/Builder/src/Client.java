a6e7b60ad8090faf9f2528d70cce27d343a0552b
Kunal Jindal <kunaljindal@Kunals-MacBook-Pro.local>
2026-03-16 23:09:30 +0530

    Prod version

--- Design Patterns/Builder/src/Client.java
+++ Design Patterns/Builder/src/Client.java
@@ -1,12 +1,16 @@
 import javax.swing.text.html.HTMLEditorKit;
+import java.awt.*;
+import java.awt.image.BufferedImageOp;
+import java.lang.reflect.WildcardType;
 
 public class Client {
     public static void main(String[] args) {
-        Helper helper = new Helper();
-        helper.setName("ABC");
-        helper.setAge(23);
-        helper.setUniversity("NIT Allahabad");
+        Student student = Student.getHelper()
+                .setName("ABC")
+                .setUniversity("NIT Allahabad")
+                .setAge(23)
+                .build();
 
-        Student s = new Student(helper);
+//        Student s = new Student(new Builder());
     }
 }
--- Design Patterns/Builder/src/Student.java
+++ Design Patterns/Builder/src/Student.java
@@ -14,24 +14,18 @@ public class Student {
 
     private double psp;
 
-    public Student(Helper helper) {
-        if(helper.getAge()==0) {
-            throw new IllegalArgumentException("Invalid age");
-        }
-        if(helper.getName()==null) {
-            throw new IllegalArgumentException("Invalid age");
-        }
-        // validations
-        this.name = helper.getName();
+    private Student(Builder builder) {
+
+        this.name = builder.getName();
         // set more values
-        if(helper.getPsp() <= 0) {
-            this.psp = helper.getPsp();
+        if(builder.getPsp() <= 0) {
+            this.psp = builder.getPsp();
         }
 
     }
 
-    public static Helper getHelper() {
-        return new Helper();
+    public static Builder getHelper() {
+        return new Builder();
     }
 
     public double getPsp() {
@@ -88,5 +82,96 @@ public class Student {
 
     public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    static class Builder {
+
+        private int id;
+
+        private String name;
+
+        private int age;
+
+        private String university;
+
+        private int gradYear;
+
+        private long phoneNumber;
+
+        private double psp;
+
+        public int getId() {
+            return id;
+        }
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public String getUniversity() {
+            return university;
+        }
+
+        public Builder setUniversity(String university) {
+            this.university = university;
+            return this;
+        }
+
+        public int getGradYear() {
+            return gradYear;
+        }
+
+        public Builder setGradYear(int gradYear) {
+            this.gradYear = gradYear;
+            return this;
+        }
+
+        public long getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public Builder setPhoneNumber(long phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public double getPsp() {
+            return psp;
+        }
+
+        public Builder setPsp(double psp) {
+            this.psp = psp;
+            return this;
+        }
+
+        public Student build() {
+            if(this.getAge()==0) {
+                throw new IllegalArgumentException("Invalid age");
+            }
+            if(this.getName()==null) {
+                throw new IllegalArgumentException("Invalid age");
+            }
+            // validations
+            return new Student(this);
+        }
     }
 }
